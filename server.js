a4451081a556dddf290cb7b5a68608070765a5b3
phil cowan <philipjcowan1@gmail.com>
2020-04-07 16:34:09 -0700

    getting some results to show up on the page

--- server.js
+++ server.js
@@ -18,11 +18,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 mongoose.connect("mongodb://localhost/newsscraper", { useNewUrlParser: true });
 
 app.get("/", function(req, res) {
-    res.send("Hello")
+    db.Article.find({}).then(function(dbArticle){
+        var hbsObject = {
+            articles: dbArticle
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject)
+    })
+    
 });
 
 app.get("/scrape", function(req, res){
@@ -48,10 +59,44 @@ app.get("/scrape", function(req, res){
                 console.log(err);
             });
         });
-        res.send("Scrape Complete")
     })
 })
 
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id})
+    .populate("note")
+    .then(function(dbArticle){
+        res.json(dbArticle);
+    })
+    .catch(function(err){
+        res.json(err)
+    })
+})
+
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+    .then(function(dbNote){
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: {note:dbNote._id }}, { new:true });
+    }).then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err){
+      res.json(err);
+    })
+  });
 
 
 app.listen(PORT, function() {
